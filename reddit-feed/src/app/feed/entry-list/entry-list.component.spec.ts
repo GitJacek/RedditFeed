bbe99cea51bebcbcee05212c2782ee3bd9a57595
unknown <jack.rasala@gmail.com>
2021-12-05 21:15:28 +0100

    Added next and previous unit tests

--- reddit-feed/src/app/feed/entry-list/entry-list.component.spec.ts
+++ reddit-feed/src/app/feed/entry-list/entry-list.component.spec.ts
@@ -1,25 +1,67 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { TestBed } from '@angular/core/testing';
 
 import { EntryListComponent } from './entry-list.component';
+import { EntryListHttpService } from './entry-list-http.service';
+import { RedditPage } from '../models/reddit-page';
+import { Observable, from } from 'rxjs';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
 
-describe('FeedComponent', () => {
+class MockEntryListHttpService {
+  getEntries(limit: number, showedEntries: number, beforeId?: string, afterId?: string): Observable<RedditPage> {
+    return from([{
+      after: 'afterId',
+      before: 'beforeId',
+      children: new Array(limit).fill(0).map((x, i) => {
+        const itemNr = showedEntries + i;
+        return {
+          data: {
+            id: `id${itemNr}`,
+            thumbnail: `id${itemNr}`,
+            created: new Date(),
+            num_comments: x,
+            author: `author${itemNr}`,
+            score: x,
+            title: `title${itemNr}`,
+            selftext: `selftext${itemNr}`
+          }
+        };
+      })
+    }])
+  }
+}
+
+describe('EntryListComponent', () => {
   let component: EntryListComponent;
-  let fixture: ComponentFixture<EntryListComponent>;
-
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ EntryListComponent ]
-    })
-    .compileComponents();
-  });
+  let entryListHttpService: EntryListHttpService;
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(EntryListComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    TestBed.configureTestingModule({
+      imports: [MatDialogModule],
+      providers: [
+        EntryListComponent,
+        { provide: EntryListHttpService, useClass: MockEntryListHttpService },
+        MatDialog
+      ]
+    });
+
+    component = TestBed.inject(EntryListComponent);
+    entryListHttpService = TestBed.inject(EntryListHttpService);
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should load next entries', () => {
+    component.loadNextEntries();
+
+    expect(component.entries[0].id).toEqual('id25');
+    expect(component.showedEntries).toEqual(25);
+  });
+
+  it('should load previous entries', () => {
+    component.loadNextEntries();
+    component.loadNextEntries();
+    component.loadNextEntries();
+    component.loadPreviousEntries();
+
+    expect(component.entries[0].id).toEqual('id50');
+    expect(component.showedEntries).toEqual(50);
   });
 });
